3087669d8acecd7ee86b2b193f50448a383ddbf1
SerVer1988 <126644477+SerVer1988@users.noreply.github.com>
2026-05-15 08:43:03 +1000

    Fix formatting of self.clients.claim() in activate event

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
-  self.clients.claim();
+  self.clients.claim(); 
 });
 
 // Network-first: всегда берём свежее из сети, кэш только при офлайн
